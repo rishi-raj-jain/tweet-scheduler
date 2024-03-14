ca59899a90097fe31227172def2e35e10b0ae329
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-14 12:25:00 +0530

    type issue

--- lib/twitter/config.ts
+++ lib/twitter/config.ts
@@ -1,8 +1,8 @@
 import { auth } from 'twitter-api-sdk'
 
 export const authClient = new auth.OAuth2User({
-  client_id: process.env.TWITTER_CLIENT_ID!,
-  callback: process.env.TWITTER_AUTH_CALLBACK_URL!,
-  client_secret: process.env.TWITTER_CLIENT_SECRET,
+  client_id: process.env.TWITTER_CLIENT_ID as string,
+  callback: process.env.TWITTER_AUTH_CALLBACK_URL as string,
+  client_secret: process.env.TWITTER_CLIENT_SECRET as string,
   scopes: ['tweet.write', 'tweet.read', 'offline.access', 'users.read'],
 })
--- lib/upstash/config.ts
+++ lib/upstash/config.ts
@@ -2,8 +2,8 @@ import { Redis } from '@upstash/redis'
 import { Queue } from '@upstash/queue'
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.UPSTASH_REDIS_REST_URL as string,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
 })
 
 export const queue = new Queue({
