e1fc5b60ad2dc530646e9a3b6da39563827f10dc
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-15 23:26:43 +0530

    import

--- app/api/auth/twitter/authenticated/route.ts
+++ app/api/auth/twitter/authenticated/route.ts
@@ -1,7 +1,7 @@
 // File: app/api/auth/twitter/authenticated/route.ts
 
-import { NextResponse } from 'next/server'
 import { redis } from '@/lib/upstash'
+import { NextResponse } from 'next/server'
 
 export async function GET() {
   const access_token = await redis.get<string>('twitter_oauth_access_token')
